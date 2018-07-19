96bcd8702fab9211a377238a8e6815a9575cc899
PC38209 <PC38209@PC38209>
2018-07-19 17:40:25 +0900

    Mybatis

---
+++ day12-Product-Mybatis/src/shop/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package shop.mapper;
+
+
+import java.util.List;
+
+import shop.vo.Product;
+
+public interface ProductMapper {
+	
+	int insert(Product product);
+	Product selectOne(Product product);
+	int update(Product product);
+	int deleteOne(Product product);
+	List<Product> selectAll();
+	int isExists(Product product);
+	
+}
